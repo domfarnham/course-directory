27c75f9449a8bca5a93956e6249bf12390d709d4
Dominic Farnham <dominic.farnham@hotmail.co.uk>
2019-01-16 08:29:46 +0000

    Implement NavLinks and additional routing for the Courses component.

--- src/components/courses/HTML.js
+++ src/components/courses/HTML.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Course from './Course';
-import { HTMLCourses } from '../../data/courses';
+import React from 'react'
+import Course from './Course'
+import { HTMLCourses } from '../../data/courses'
 
 const HTML = (props) => {
   let courses = HTMLCourses.map((course) => {
     return <Course title={course.title}
-                   desc={course.description}
-                   img={course.img_src}
-                   key={course.id} />
-  }); 
+      desc={course.description}
+      img={course.img_src}
+      key={course.id} />
+  })
   return (
     <div>
       <ul>
-        {courses}    
+        {courses}
       </ul>
     </div>
-  );
+  )
 }
 
-export default HTML;
+export default HTML
